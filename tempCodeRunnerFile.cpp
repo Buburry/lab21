5bc1cb048103ea7e663006c32e933e3274fed27d
Buburry <Natapon7934@gmail.com>
2024-02-20 17:10:27 +0700

    RUN in ที่อื่นได้

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,4 @@
+// [Missing Code 1] Include header file.
 #include"lab21.h"
   
 int main(){
@@ -9,6 +10,7 @@ int main(){
 	Unit hero("Hero",name);
 	
 	Equipment sword(0,8,4);
+	// [Missing Code 2]  Create Equipment axes, shield and armor here
 	Equipment axes(0,16,-3);
 	Equipment shield(0,-1,7);
 	Equipment armor(25,-2,2);
@@ -18,16 +20,21 @@ int main(){
 	cout << " [1] Sword \n [2] Axes \n [3] Shield \n [4] Armor \n";
 	cout << "Please selet your equipment: ";
 	cin >> eq;
-	 
-if (eq == '1') {
-    hero.equip(&sword);
-} else if (eq == '2') {
-    hero.equip(&axes);
-} else if (eq == '3') {
-    hero.equip(&shield);
-} else if (eq == '4') {
-    hero.equip(&armor);
-}
+	// [Missing Code 3] Equip a selected equipment to the hero. 
+	if (eq == 1)
+	{
+		hero.equip(&sword);
+	} else if (eq == 2)
+	{
+		hero.equip(&axes);
+	} else if (eq == 3)
+	{
+		hero.equip(&shield);
+	} else if (eq == 4)
+	{
+		hero.equip(&armor);
+	}
+	
 	
 	Unit mons("Monster","Kraken");
 	
@@ -66,15 +73,19 @@ if (eq == '1') {
 			cout << "Please select your equipment: ";
 			cin >> eq;
 			// [Missing Code 3] Equip a selected equipment to the hero.
-if (eq == '1') {                     //ตอนแรกพลาด เพราะ eq == 1 ให้เท่ากับ 1 ซึ่งเป็น int ต้องเป็น char ถึงจะถูก
-    hero.equip(&sword);
-} else if (eq == '2') {
-    hero.equip(&axes);
-} else if (eq == '3') {
-    hero.equip(&shield);
-} else if (eq == '4') {
-    hero.equip(&armor);
-}
+				if (eq == 1)
+	{
+		hero.equip(&sword);
+	} else if (eq == 2)
+	{
+		hero.equip(&axes);
+	} else if (eq == 3)
+	{
+		hero.equip(&shield);
+	} else if (eq == 4)
+	{
+		hero.equip(&armor);
+	}
 			
 		}
 		
